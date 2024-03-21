80c6e89e3347c541430e70574882ca5203437d25
Bekzhan <114474251+rakh1mbayev@users.noreply.github.com>
2024-03-21 12:51:22 +0500

    Add files via upload

---
+++ awesomeProject/Functionality/ListTasks.go
@@ -0,0 +1,20 @@
+package Functionality
+
+import (
+	"fmt"
+)
+
+func (x *Tasks) ListTasks() {
+	if len(x.tasks) == 0 {
+		fmt.Println("No tasks found.")
+		return
+	}
+	fmt.Println("Tasks:")
+	for _, task := range x.tasks {
+		status := "Complete"
+		if !task.Complete {
+			status = "Incomplete"
+		}
+		fmt.Println(task.ID, task.Name, status)
+	}
+}
---
+++ awesomeProject/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"Functionality"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var tasks Functionality.Tasks
+
+func main() {
+	fmt.Println("Welcome to To-Do List App!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Enter command (add/list/update/delete/exit): ")
+		scanner.Scan()
+		command := strings.ToLower(scanner.Text())
+		switch command {
+		case "add":
+			fmt.Println("Enter task name: ")
+			scanner.Scan()
+			taskName := scanner.Text()
+			tasks.AddTask(taskName)
+		case "list":
+			tasks.ListTasks()
+		case "update":
+			fmt.Println("Enter task ID to update: ")
+			scanner.Scan()
+			taskID := 0
+			fmt.Scan(scanner.Text(), &taskID)
+			tasks.UpdateTask(taskID)
+		case "delete":
+			fmt.Println("Enter task ID to delete: ")
+			scanner.Scan()
+			taskID := 0
+			fmt.Scan(scanner.Text(), &taskID)
+			tasks.DeleteTask(taskID)
+		case "exit":
+			fmt.Println("Exiting...")
+			break
+		default:
+			fmt.Println("Invalid command. Please try again.")
+		}
+
+	}
+}
